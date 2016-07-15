707f35aefd7ad38eda41c9cd81494c01ebad0afc
vviimm <wowaonrails@gmail.com>
2016-07-15 19:48:53 +0300

    Debug

--- app/mailers/hit_counter_mailer.rb
+++ app/mailers/hit_counter_mailer.rb
@@ -1,9 +1,13 @@
 class HitCounterMailer < ApplicationMailer
 
   def while_in_vkontact(user)
+    Rails.loger.info "-------------------------------------Mailer Start"
+
     @vk_user = user
     @user = User.first
     mail(to: "Vladimir <#{@user.email}>",
       subject: "Время нахождения в VK: #{user.duration(user.times_per_day)}")
+
+    Rails.loger.info "-------------------------------------Mailer Finish"
   end
 end
--- app/workers/vk_worker.rb
+++ app/workers/vk_worker.rb
@@ -1,8 +1,12 @@
 class VkWorker
   include Sidekiq::Worker
 
-  def perform(user_id)
+  def perform(user_id)\
+    Rails.loger.info "-------------------------------------Worker Start"
+
     user = VkUser.find(user_id)
     HitCounterMailer.while_in_vkontact(user).deliver
+
+    Rails.loger.info "-------------------------------------Worker Finish"
   end
 end
